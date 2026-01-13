bbe92deff8a37c1b40c20a6482052459591a48ff
Marcelo Costa <costanetomj@gmail.com>
2026-01-13 18:00:15 -0300

    Initial Commit

---
+++ runrunapp/runrunappApp.swift
@@ -0,0 +1,17 @@
+//
+//  runrunappApp.swift
+//  runrunapp
+//
+//  Created by Marcelo Costa on 13/01/26.
+//
+
+import SwiftUI
+
+@main
+struct runrunappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
